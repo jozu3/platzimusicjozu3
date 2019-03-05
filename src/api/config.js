7c22ec8d84b771364a814bdf55a4e34ee2866da5
jozu3 <josue.vitate@gmail.com>
2019-03-05 15:45:20 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default{
+	appName: 'platzimusic',
+	apiKey: 'eeac057c448d66051a85c716d32e0ee5',
+	secret: 'cc27479c8c2957fe68d8ba8b80fab6bd',
+	registered: 'jozu3vi'
+}
+
+
+//Application name	platzimusic
+//API key	eeac057c448d66051a85c716d32e0ee5
+//Shared secret	cc27479c8c2957fe68d8ba8b80fab6bd
+//Registered to	jozu3vi
