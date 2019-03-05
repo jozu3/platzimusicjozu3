ed05c1892c15cafe1c41e4050cf4f45890e6f3ed
jozu3 <josue.vitate@gmail.com>
2019-03-05 17:16:36 -0500

    Fixes por la url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://jozu3.github.io/platzimusicjozu3/dist/logo.png')
     h1 {{ title }}
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
